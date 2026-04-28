3d352d442b6ea42f373f2df2fb4353e66354eba0
Sidnei Rocha da Silva Barros <sidneibarros@Mac-mini-de-Sidnei.local>
2026-04-28 11:49:55 -0300

    fix: revert to using json config for firebase to resolve AWS production error

--- firebase.ts
+++ firebase.ts
@@ -2,19 +2,10 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-};
+// Import the Firebase configuration
+import firebaseConfig from './firebase-applet-config.json';
 
 // Initialize Firebase SDK
 const app = initializeApp(firebaseConfig);
-const dbId = import.meta.env.VITE_FIREBASE_FIRESTORE_DATABASE_ID;
-// Se for "default", converte para undefined para usar o banco padrão (default)
-const finalDbId = (dbId === 'default' || !dbId) ? undefined : dbId;
-export const db = getFirestore(app, finalDbId);
+export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 export const auth = getAuth(app);
